a2e666e70aa079c3e247614ee146d0cf8aef2a9a
Matteo Piccini <gnappo.1@live.it>
2019-10-17 02:39:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,9 @@
 class Student 
   def hello
-    return "Hey there! I'm so excited to learn stuff.\n"
+    return "Hey there! I'm so excited to learn stuff."
   end
   
   def raise_hand
-    
+    return "Pick me!"
   end
 end
